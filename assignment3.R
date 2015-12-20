55f0740d5d104a06d655224b2c9f4a168dc7f412
Smorni <ule.turbert@gmail.com>
2015-12-20 02:21:47 +0100

    Added some test data

--- assignment3.R
+++ assignment3.R
@@ -3,11 +3,28 @@
 
 require(Rcpp)
 require(RcppArmadillo)
+require(adagio)
+
+p <- c(15, 100, 90, 60, 40, 15, 10,  1)
+w <- c( 2,  20, 20, 30, 40, 30, 60, 10)
+cap <- 102
+
+p <- c(70, 20, 39, 37, 7, 5, 10)
+w <- c(31, 10, 20, 19, 4, 3,  6)
+cap <- 50
 
 system.time(
-  rescpp <- ska(objWeight = testW,
-                testV,
-                maxW,
+  rescpp <- ska(w,
+                p,
+                cap,
                 10000,
                 1000000)
 )
+
+system.time(
+  resada <- knapsack(w,
+                     p,
+                     cap)
+)
+
+
